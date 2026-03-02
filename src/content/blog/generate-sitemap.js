ea5bf02c949a6789895b446d30c3d0181a013dac
Prodyut Acharya <bipulbala64@gmail.com>
2026-03-03 01:12:09 +0530

    Create generate-sitemap.js

---
+++ src/content/blog/generate-sitemap.js
@@ -0,0 +1,38 @@
+const fs = require('fs');
+const path = require('path');
+
+const BASE_URL = 'https://astro.myastrology.in';
+const OUTPUT = path.join(process.cwd(), 'public', 'my-sitemap.xml');
+
+function readBlogPosts() {
+  const blogPath = path.join(process.cwd(), 'src/content/blog/list.json');
+  try {
+    const posts = JSON.parse(fs.readFileSync(blogPath, 'utf8'));
+    return posts.map(post => ({
+      url: `${BASE_URL}/blog.html?post=${post.slug}`,
+      lastmod: post.date
+        ? new Date(post.date).toISOString().split('T')[0]
+        : new Date().toISOString().split('T')[0],
+      changefreq: 'monthly',
+      priority: 0.85
+    }));
+  } catch (err) {
+    console.warn('list.json read failed:', err.message);
+    return [];
+  }
+}
+
+const allUrls = readBlogPosts();
+
+const xml = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+${allUrls.map(p => `  <url>
+    <loc>${p.url}</loc>
+    <lastmod>${p.lastmod}</lastmod>
+    <changefreq>${p.changefreq}</changefreq>
+    <priority>${p.priority}</priority>
+  </url>`).join('\n')}
+</urlset>`;
+
+fs.writeFileSync(OUTPUT, xml, 'utf8');
+console.log(`${allUrls.length} blog posts done`);
